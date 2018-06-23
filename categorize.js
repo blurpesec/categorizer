cb1e99cf33c6724f3731748157b520bada12c734
hahnmichaelf <hahn.michael.f@gmail.com>
2018-06-22 21:43:54 -0400

    update

--- categorize.js
+++ categorize.js
@@ -17,7 +17,13 @@ var str = '';
 var totalchecks = 0;
 var totalphishing = 0;
 var totalscamming = 0;
-var totals = {"phishing": 0,"scamming": 0}
+var totalfakeico = 0;
+var totals =
+[
+  {"phishing": 0},
+  {"scamming": 0},
+  {"fakeico": 0}
+]
 var scamslist = [];
 var notphish = [];
 var notphishorscam = [];
@@ -45,12 +51,14 @@ function initialize(){
       if(checkToken(newstring, str)){
         //console.log(str) + " was detected");
       }
-
     }
   }
+
   console.timeEnd("test");
-  console.log("Total number of phishing category: " + totalphishing);
-  console.log("Total number of scamming category: " + totalscamming);
+  console.log("Total number of phishing category: " + totalphishing + " = " + totals[0].phishing + " / " + scamslist.length + " | " + totalphishing/scamslist.length);
+  console.log("Total number of scamming category: " + totalscamming + " = " + totals[1].scamming + " / " + scamslist.length + " | " + totalscamming/scamslist.length);
+  console.log("Total number of fake ico category: " + totalfakeico + " = " + totals[2].scamming + " / " + scamslist.length + " | " + totalfakeico/scamslist.length);
+
 
   console.log("Total number of positives: " + (totalphishing + totalscamming));
   console.log("Total number of negatives: " + notphishorscam.length);
@@ -60,6 +68,7 @@ function initialize(){
 
   console.log("Length of scamslist: " + scamslist.length);
   console.log("Categorizations still needed: " + notphishorscam.length);
+  //console.log("Totals: " + JSON.stringify(totals, null, 2))
   fs.writeFileSync('./scamslist.json', JSON.stringify(scamslist, null, 4), function(e,results){
     if(e) console.log(e);
     else{
@@ -67,10 +76,11 @@ function initialize(){
       fs.close();
     }
   });
+
   fsTwo.writeFileSync('./notphishorscam.json', JSON.stringify(notphishorscam, null, 4), function(e,results){
     if(e) console.log(e);
     else{
-      //console.log("Done writing to notphishorscam.json");
+        //console.log("Done writing to notphishorscam.json");
       fsTwo.close();
     }
   });
@@ -105,6 +115,7 @@ function checkLevenshtein(input, str){
   }
   if(smallestleven <= lengthtotest){
     totalphishing += 1;
+    totals[0].phishing += 1;
     scamslist.push(str);
     return true;
   }
@@ -114,20 +125,51 @@ function checkLevenshtein(input, str){
   }
 }
 
-async function checkToken(input, str){
-  for(var y = 0; y < tokenjson.tokens.length; y ++){
-    for(var z = 0; z < tokenjson.tokens[y].tokens.length; z++){
-      if(input.indexOf(tokenjson.tokens[y].tokens[z]) > -1){
-        scamslist.push(str);
-        var cat = tokenjson.tokens[y].category.toLowerCase();
-        if(cat = "scamming"){
-          totalscamming += 1;
-          return true;
+function checkToken(input, str){
+
+  for(var y = 0; y < tokenjson.cards.length; y ++){
+    for(var z = 0; z < tokenjson.cards[y].tokens.length; z++){
+      if(input.indexOf(tokenjson.cards[y].tokens[z]) > -1){
+        /*if(tokenjson.tokens[y].continue == true){
+          // Add scam to scamslist, chang the chain to
         }
-        else if(cat = "phishing"){
-          totalphishing += 1;
-          return true;
-        }
+        else{*/
+          scamslist.push(str);
+          //var cat = tokenjson.tokens[y].category.toLowerCase();
+          var subcat = tokenjson.cards[y].subcategory.toLowerCase();
+          var name = tokenjson.cards[y].name.toLowerCase();
+          //console.log("looking for: " + subcat + " in " + totals.count[1].phishing)\
+
+        //for(var o = 0; o < 50; o++){
+          /*if(subcat.indexOf(totals)!= 0){
+            console.log("wat da fuck")
+          }
+          if(subcat.indexOf(totals) = 0){*/ // if subcategory exists in totals, add +1 ; always false;
+            if(tokenjson.cards[y].category = "scamming"){
+              //console.log(JSON.stringify(tokenjson.tokens[y], null, 2))
+              totals[1].scamming += 1;
+              totalscamming += 1;
+              return true;
+            }
+            if(tokenjson.cards[y].category = "phishing"){
+              totals[0].phishing += 1;
+              totalphishing += 1;
+              return true;
+            }
+            if(tokenjson.cards[y].category = "fakeico"){
+              //console.log(input + " was detected as a fakeico category and subcategory: " + subcat + ". Under name: " + name);
+              totals[2].fakeico += 1;
+              totalfakeico += 1;
+              return true;
+            }
+            /*
+          }
+          else{
+            totals.count.push( {[subcat]: 1} );
+          }
+        }*/
+
+        /*}*/
       }
     }
   }
